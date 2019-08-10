61aec90b8aa8f72dfdbdb1e55197af3582f00917
Stalemate99 <36290185+Stalemate99@users.noreply.github.com>
2019-08-10 18:33:55 +0530

    only balls left

--- src/js/components/animation.js
+++ src/js/components/animation.js
@@ -3,14 +3,40 @@ import '../../css/animation.css';
 
 class Animation extends React.Component {
 
+    componentDidMount() {
+        Array
+            .from(document.querySelectorAll("span"))
+            .reverse()
+            .forEach((x) => {
+                setTimeout(() => {
+
+                    x.style = `transition: 2.5s ease;opacity:0`;
+                }, 1000)
+            });
+
+        document
+            .querySelector("#title")
+            .style = "opacity:0";
+        document
+            .querySelector("#bulb")
+            .style = "opacity:0";
+
+        setTimeout(() => {
+            document
+                .querySelector("#title")
+                .style = "opacity:1;transition:2s ease-in;position:absolute;top:50vh;left:25vw;width:50vw;" +
+                    "height:50vh"
+        }, 4000);
+        setTimeout(() => {
+            document
+                .querySelector("#bulb")
+                .style = "opacity:1;animation : blink 3s;tranisition: 3s ease-in;position:absolute;top:30v" +
+                    "h;left:32vw;width:35vw;height:35vh"
+        }, 2000);
+    }
+
     render = () => {
-        const bulb = {
-            position:"absolute",
-            top:"30vh",
-            left:"32vw",
-            width:"35vw",
-            height:"35vh"
-        };
+
         const rball = {
             position: "absolute",
             top: "-8vh",
@@ -20,64 +46,59 @@ class Animation extends React.Component {
             overflow: "hidden"
         };
         const lball = {
-            position:"absolute",
-            top:"-8vh",
-            left:"54vw",
-            width:"78vw",
-            height:"72vw",
-            overflow:"hidden"
+            position: "absolute",
+            top: "-8vh",
+            left: "54vw",
+            width: "78vw",
+            height: "72vw",
+            overflow: "hidden"
         };
         const sdiv = {
-            overflow:"hidden",
-            position:"realtive"
-        }
-        const title = {
-            position:"absolute",
-            transform:"translate3d(27vw,50vh,10px)",
-            width:"50vw",
-            height:"50vh"
+            overflow: "hidden",
+            position: "realtive"
         }
         const bg = {
-            width:"100vw",
-            height:"100vh"
+            width: "100vw",
+            height: "100vh"
         }
-        
+
         return (
             <div style={sdiv}>
                 <img src={require("../../svg/Main_background.svg")} alt="bg" style={bg}/>
-                <img src={require("../../svg/Right_ball.svg")} alt="rball" style={rball} />
-                <img src={require("../../svg/Left_ball.svg")} alt="lball" style={lball} />
-                <img src={require("../../svg/Logo_bulb.svg")} alt="logo" style={bulb} className="bulb"/>
-                <img src={require("../../svg/logo_text_with_tagline.svg")} alt="title" style={title}/>
-                <div class="herosplash1">
-        <div id="herosplash" class="commondiv">
-            <div id="hypno" class="commondiv">
-                <div id="lastdiv" class="commondiv">
-                    <span class="startspan">
-                        <span class="startspan">
-                            <span class="startspan">
-                                <span class="startspan">
-                                    <span class="startspan">
-                                        <span class="startspan">
-                                            <span class="startspan">
-                                                <span class="startspan">
-                                                    <span class="startspan">
-
+                <img src={require("../../svg/Right_ball.svg")} alt="rball" style={rball}/>
+                <img src={require("../../svg/Left_ball.svg")} alt="lball" style={lball}/>
+                <img src={require("../../svg/Logo_bulb.svg")} alt="logo" id="bulb"/>
+                <img
+                    src={require("../../svg/logo_text_with_tagline.svg")}
+                    alt="title"
+                    id="title"/>
+                <div className="herosplash1">
+                    <div id="herosplash" className="commondiv">
+                        <div id="hypno" className="commondiv">
+                            <div id="lastdiv" className="commondiv">
+                                <span className="startspan">
+                                    <span className="startspan">
+                                        <span className="startspan">
+                                            <span className="startspan">
+                                                <span className="startspan">
+                                                    <span className="startspan">
+                                                        <span className="startspan">
+                                                            <span className="startspan">
+                                                                <span className="startspan"></span>
+                                                            </span>
+                                                        </span>
                                                     </span>
                                                 </span>
                                             </span>
                                         </span>
                                     </span>
                                 </span>
-                            </span>
-                        </span>
-                    </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-            </div>
-            
+
         )
     }
 }
